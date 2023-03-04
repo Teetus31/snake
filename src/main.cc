4e4ab5680aab5146544e30f015c92c26fbfde56a
Teetus31 <titusclash31@gmail.com>
2023-03-04 20:45:07 +0200

    finished the game, made the snake longer

--- src/main.cc
+++ src/main.cc
@@ -30,17 +30,21 @@ struct snake {
 
     void draw(std::vector<console::Pixel> & pixels, std::size_t X, std::size_t) {
         std::size_t cnt = 0;
-        for(auto & segment : segments)
+        for(auto segment = segments.rbegin(); segment != segments.rend(); segment++)
             if(cnt++ == 0)
-                console::grid::at_2D(pixels, segment.x, segment.y, X) = console::Pixel(fg::WHITE, bg::BLACK, '0');
+                console::grid::at_2D(pixels, segment->x, segment->y, X) = console::Pixel(fg::WHITE, bg::BLACK, '0');
             else
-                console::grid::at_2D(pixels, segment.x, segment.y, X) = console::Pixel(fg::WHITE, bg::BLACK, 'o');
+                console::grid::at_2D(pixels, segment->x, segment->y, X) = console::Pixel(fg::WHITE, bg::BLACK, 'o');
     }
 
     void move(std::size_t direction, std::size_t X, std::size_t Y) {
         if (dead == true) {
             return ;
         }
+
+        block first = *segments.rbegin();
+        segments.push_back(first);
+
         if(direction == 0) { // up
             segments.rbegin()->y--;
         } else if(direction == 1) { // down
@@ -55,8 +59,28 @@ struct snake {
             dead = true;
         if(segments.rbegin()->y == 0 || segments.rbegin()->y > Y - 1)
             dead = true;
+
+        if(segments.rbegin()->x == apple.coord.x && segments.rbegin()->y == apple.coord.y){
+            score += 10;
+            apple.coord.x = rand() % (X - 1);
+            apple.coord.y = rand() % (Y - 2);
+        }
+        else {
+            segments.pop_front();
+        }
+
+        for(auto poz = ++segments.rbegin(); poz != segments.rend(); ++poz) {
+            if(poz->x == segments.rbegin()->x && poz->y == segments.rbegin()->y) {
+                direction = 5;
+                dead = true;
+            }
+
+        }
+
+
     }
 };
+
 snake player;
 
 std::size_t direction = 5;
@@ -68,14 +92,23 @@ std::size_t initial_value_x, initial_value_y;
 void keys(char key) {
     if(dead == false) {
 
-        if(key == 'w')
+        if(key == 'w'){
+            if(direction != 1)
             direction = 0;
-        else if(key == 's')
+        }
+        else if(key == 's'){
+            if(direction != 0)
             direction = 1;
-        else if(key == 'a')
+        }
+        else if(key == 'a'){
+            if(direction != 3)
             direction = 2;
-        else if(key == 'd')
+        }
+        else if(key == 'd'){
+            if(direction != 2)
             direction = 3;
+
+        }
     }
 
     if(key == 'q')
@@ -127,8 +160,8 @@ void score(std::vector<console::Pixel> & pixels, std::size_t X, std::size_t Y) {
         console::col::ITALIC::NO,
         console::col::UNDERLINE::NO,
         console::col::STRIKETHROUGH::NO,
-        2,
-        5,
+        3,
+        0,
         X
     );
 }
@@ -174,13 +207,13 @@ bool Update(std::vector<console::Pixel> & pixels, std::size_t X, std::size_t Y, 
     initial_value_y = Y;
     console::grid::for_each_0(pixels, X, Y, make_black);
     drawBorder(pixels, X, Y);
-
+    player.draw(pixels, X, Y);
 
     if(dead) {
         direction = 5;
         death_screen(pixels, X, Y);
     }
-    player.draw(pixels, X, Y);
+
     acumulator = acumulator + deltatime;
 
     while(acumulator >= tickrate) {
